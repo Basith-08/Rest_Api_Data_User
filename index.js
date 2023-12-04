f921716c59abac54a7631e8cea06ccbce4a28199
Basith-08 <asfinebasith@gmail.com>
2023-12-04 10:56:48 +0700

    Initital commit

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import UserRouter from "./routes/UserRoutes.js";
+
+import 'dotenv/config'
+
+const app = express();
+// Connecet to mongoose
+mongoose.connect(process.env.REACT_DB)
+const db = mongoose.connection;
+db.on('error', error => console.log(error))
+db.once('open', () => console.log('Database connected... '))
+
+// Midleware
+app.use(cors())
+app.use(express.json()) // Menerima data dalam format JSON
+app.use(UserRouter)
+
+app.listen(5000, () => console.log('Server up and running...'))
